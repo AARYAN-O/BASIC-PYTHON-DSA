197dae2c9515eea7075736287efa87f630226d20
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-10-03 16:51:55 +0530

    Create Create_and_print_bst.py

---
+++ Create_and_print_bst.py
@@ -0,0 +1,54 @@
+class Node:
+    def __init__(self,key):
+        self.left=None
+        self.right=None
+        self.val=key
+    
+def insert(root,key):
+    if root is None:
+        return Node(key)
+    # create new Node if root is None
+    else:
+        if root.val<key:
+            root.right=insert(root.right,key)
+        else:
+            root.left=insert(root.left,key)
+    return root 
+
+def inorder(root):
+    if root:
+        inorder(root.left)
+        print(root.val,end=" ")
+        inorder(root.right)
+    
+def preorder(root):
+    if root:
+        print(root.val,end=" ")
+        preorder(root.left)
+        preorder(root.right)
+    
+def postorder(root):
+    if root:
+        postorder(root.left)
+        postorder(root.right)
+        print(root.val,end=" ")
+
+root=Node(10)
+root=insert(root,20)
+root=insert(root,30)
+root=insert(root,40)
+
+print("inorder traversal")
+inorder(root)
+print()
+
+print("post order traversal")
+postorder(root)
+print()
+
+print("preorder traversal")
+preorder(root)
+print()
+
+
+    
